53ba8695225bd0fa8a0208230da9fcbcab6f1636
ThetHtooZaw <thethtoozaw@blue-stone.net>
2025-01-08 09:29:02 +0630

    {first-commit}

---
+++ src/test/java/org/thz/schoolmanagmentsystem/SchoolManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thz.schoolmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
